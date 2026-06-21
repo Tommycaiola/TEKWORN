72c5726b078c3b7e5c4ca247c793a65239b886ca
Tommaso Caiola <tommasocaiola@MacBook-Air-di-Tommaso.local>
2026-06-22 00:31:26 +0200

    variabili settate e js configurato

--- functions/api/contact.js
+++ functions/api/contact.js
@@ -1,6 +1,22 @@
-export async function onRequest() {
-  return Response.json({
-    success: true,
-    message: "API attiva"
-  });
+export async function onRequest(context) {
+
+  const { env } = context;
+
+  const response = await fetch(
+    `https://api.telegram.org/bot${env.TELEGRAM_TOKEN}/sendMessage`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        chat_id: env.TELEGRAM_CHAT_ID,
+        text: "✅ Test Cloudflare Pages → Telegram"
+      })
+    }
+  );
+
+  const data = await response.json();
+
+  return Response.json(data);
 }
